7519523fb9d777224f6e7ef3770373db8fce4169
Eligardo Cruz Marin <elcruzma@ittepic.edu.mx>
2023-01-23 11:58:06 -0700

    cambio en migracion bd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('rfc')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('type');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
