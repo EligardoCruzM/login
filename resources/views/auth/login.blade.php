d0d4a23b3e9c1c4f96c898bfaf5417b33d2a93c4
Eligardo Cruz Marin <elcruzma@ittepic.edu.mx>
2023-01-24 12:22:43 -0700

    correciones de sesión

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                <pre>{{ Auth::user() }}</pre>
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index')->middleware('auth');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/index', [App\Http\Controllers\HomeController::class, 'index'])->name('index')->middleware('auth');
